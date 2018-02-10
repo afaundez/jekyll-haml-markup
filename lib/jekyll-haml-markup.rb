73cb3faaed0fa4d2629c4ea1c66437def5adfecf
Alvaro Faundez <alvaro@faundez.net>
2018-02-10 15:32:40 -0500

    Haml not escaping characters, Fixes #1

--- lib/jekyll-haml-markup.rb
+++ lib/jekyll-haml-markup.rb
@@ -1,7 +1,6 @@
 require 'jekyll-haml-markup/version'
 
 require 'jekyll'
-require 'haml'
 
 require_relative 'jekyll/converters/haml'
 require_relative 'jekyll/haml/parser'
@@ -10,19 +9,35 @@ require_relative 'jekyll/tags/include'
 
 
 Jekyll::Hooks.register :site, :post_read do |site|
+  haml_converter = Jekyll::Converters::Haml.new site.config
   site.layouts.each do |name, layout|
-    layout.content = ::Haml::Engine.new(layout.content).render if ['.haml'].include? layout.ext
+    if haml_converter.matches layout.ext
+      parsed_content = haml_converter.convert layout.content
+      layout.content = parsed_content
+    end
   end
 end
 
-Jekyll::Hooks.register :pages, :pre_render do |page|
-  page.content = ::Haml::Engine.new(page.content).render if ['.haml'].include? page.ext
-end
-
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  post.content = ::Haml::Engine.new(post.content).render if ['.haml'].include? post.ext
-end
-
-Jekyll::Hooks.register :documents, :pre_render do |document|
-  document.content = ::Haml::Engine.new(document.content).render if ['.haml'].include? document.ext
-end
+# Jekyll::Hooks.register :pages, :pre_render do |page|
+#   haml_converter = Jekyll::Converters::Haml.new page.site.config
+#   if haml_converter.matches page.ext
+#     parsed_layout = haml_converter.convert page.content
+#     page.content = parsed_layout
+#   end
+# end
+#
+# Jekyll::Hooks.register :posts, :pre_render do |post|
+#   haml_converter = Jekyll::Converters::Haml.new post.site.config
+#   if haml_converter.matches post.ext
+#     parsed_layout = haml_converter.convert post.content
+#     post.content = parsed_layout
+#   end
+# end
+#
+# Jekyll::Hooks.register :documents, :pre_render do |document|
+#   haml_converter = Jekyll::Converters::Haml.new document.site.config
+#   if haml_converter.matches document.ext
+#     parsed_layout = haml_converter.convert document.content
+#     document.content = parsed_layout
+#   end
+# end
--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -1,11 +1,18 @@
 require 'jekyll/tags/include'
+require 'jekyll/converters/haml'
 
 module Jekyll
   module Tags
     class IncludeTag < Liquid::Tag
 
       def read_file(file, context)
-        ::Haml::Engine.new(File.read(file, file_read_opts(context))).render
+        content = File.read file, file_read_opts(context)
+
+        config = context.registers[:site].config
+        haml_converter = Jekyll::Converters::Haml.new config
+        content = haml_converter.convert(content) if haml_converter.matches File.extname(file)
+
+        content
       end
 
     end
