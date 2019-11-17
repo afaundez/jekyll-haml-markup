a18280ef1a4991b92173f2e9e0c3ca28ec9433e0
Alvaro Faundez <alvaro@faundez.net>
2019-11-17 14:17:41 -0500

    Refactor tests

--- test/jekyll/haml/markup_test.rb
+++ test/jekyll/haml/markup_test.rb
@@ -20,9 +20,12 @@ describe Jekyll::Haml::Markup do
     html = Nokogiri::HTML File.read File.join(@destination, 'index.html')
     html.xpath('/html/head').wont_be_empty
     html.xpath('/html/body').wont_be_empty
-    html.xpath('/html/body/header/nav/ul/li/a').wont_be_empty
+    html.xpath('/html/body/header/nav/ul/li').size.must_equal 3
+    html.xpath('/html/body/header/nav/ul/li/a').size.must_equal 2
+    html.xpath('/html/body/header/nav/ul/li/p/a').size.must_equal 1
+    html.xpath('/html/body/main/section/article').size.must_equal 2
     html.xpath('/html/body/main/h1').wont_be_empty
-    html.xpath('/html/body/footer/div').wont_be_empty
+    html.xpath('/html/body/footer/div/p').wont_be_empty
     html.xpath('/html/body/script').wont_be_empty
   end
 end
